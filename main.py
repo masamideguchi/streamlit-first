4043465ea90bd402d78af1fcb92727d9d2e69744
masamideguchi <masami.deguchi@gmail.com>
2022-06-11 19:35:41 +0900

    2nd commit

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import streamlit as st
-# import numpy as np
-# import pandas as pd
+import numpy as np
+import pandas as pd
 
 import time
 
 st.title("Streamlit超入門")
 
-latest_iteration = st.empty()
-bar = st.progress(0)
-for i in range(100):
-    latest_iteration.text(f'Iteration {i+1}')
-    bar.progress(i + 1)
-    time.sleep(0.1)
+# latest_iteration = st.empty()
+# bar = st.progress(0)
+# for i in range(100):
+#     latest_iteration.text(f'Iteration {i+1}')
+#     bar.progress(i + 1)
+#     time.sleep(0.1)
 
 'Done !!!!!!!'
 
